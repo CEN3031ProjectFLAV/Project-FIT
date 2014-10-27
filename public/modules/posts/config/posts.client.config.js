88b4b3c4e6d6a3aaa71a585808a352e28f2f651e
Matosvictor22 <matosvictor22@gmail.com>
2014-10-27 11:23:54 -0400

    Cleaned up and added a bit of content

--- public/modules/posts/config/posts.client.config.js
+++ public/modules/posts/config/posts.client.config.js
@@ -4,8 +4,8 @@
 angular.module('posts').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Posts', 'posts', 'dropdown', '/posts(/create)?');
-		Menus.addSubMenuItem('topbar', 'posts', 'List Posts', 'posts');
-		Menus.addSubMenuItem('topbar', 'posts', 'New Post', 'posts/create');
+	//	Menus.addMenuItem('topbar', 'Posts', 'posts', 'dropdown', '/posts(/create)?');
+	//	Menus.addSubMenuItem('topbar', 'posts', 'List Posts', 'posts');
+	//	Menus.addSubMenuItem('topbar', 'posts', 'New Post', 'posts/create');
 	}
 ]);
