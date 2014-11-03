1a13c5d986ec0bb284edd399c4c4f4aecfe32a49
Vmatos <matosvictor22@gmail.com>
2014-11-03 08:21:05 -0500

    Made changes to signups
    added second part and added progress bar + configured available toolbar
    at the top.

--- public/modules/friends/config/friends.client.config.js
+++ public/modules/friends/config/friends.client.config.js
@@ -4,8 +4,8 @@
 angular.module('friends').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Friends', 'friends', 'dropdown', '/friends(/create)?');
-		Menus.addSubMenuItem('topbar', 'friends', 'List Friends', 'friends');
-		Menus.addSubMenuItem('topbar', 'friends', 'New Friend', 'friends/create');
+		Menus.addMenuItem('topbar', 'Friends', 'friends', '/friends(/create)?');
+		//Menus.addSubMenuItem('topbar', 'friends', 'List Friends', 'friends');
+		//Menus.addSubMenuItem('topbar', 'friends', 'New Friend', 'friends/create');
 	}
 ]);
--- public/modules/searchusers/config/searchusers.client.config.js
+++ public/modules/searchusers/config/searchusers.client.config.js
@@ -4,8 +4,8 @@
 angular.module('searchusers').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Searchusers', 'searchusers', 'dropdown', '/searchusers(/create)?');
-		Menus.addSubMenuItem('topbar', 'searchusers', 'List Searchusers', 'searchusers');
-		Menus.addSubMenuItem('topbar', 'searchusers', 'New Searchuser', 'searchusers/create');
+		Menus.addMenuItem('topbar', 'Search Users', 'searchusers', '/searchusers(/create)?');
+		//Menus.addSubMenuItem('topbar', 'searchusers', 'List Searchusers', 'searchusers');
+		//Menus.addSubMenuItem('topbar', 'searchusers', 'New Searchuser', 'searchusers/create');
 	}
 ]);
